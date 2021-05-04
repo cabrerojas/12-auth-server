d5fdf16c93219d09496b2315b820c063e87b80f4
cabrerojas <gcabrerojas@gmail.com>
2021-05-04 14:01:40 -0400

    crear usuario en BD
    lógica para guardar un nuevo usuario en la BD

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,57 @@
 
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario =  (req, res = response) => {
-
-    
+const crearUsuario = async(req, res = response) => {
 
     const { nombre, correo, contraseña } = req.body;
-    
+
+    try {
+
+        // Verificar el correo
+
+        const usuario = await Usuario.findOne({ correo });
+
+        if ( usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya existe. ¿ Tendrás un clon malvado ?'
+            });
+        }
+
+        // Crear usuario con el modelo
+
+        const dbUser = new Usuario( req.body );
+
+        // Hashear la contraseña
+
+        // Generar el KWT
+
+        // Crear usuario de BD
+
+        await dbUser.save();
+        
+        // Generar respuesta existosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            nombre,
+            msg: 'Usuario creado exitosamente'
+        });
+        
+
+    } catch (error) {
+
+        console.log(error);
+
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+
+   
+
     return res.json({
         ok: true,
         msg: 'Crear Usuario /nuevo'
