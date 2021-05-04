84acd1b2967ac1fd8a5d9f75c0098a7bc4c438ed
cabrerojas <gcabrerojas@gmail.com>
2021-05-04 00:16:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 12-auth-server
+ Backend en para manejar un login con JWT utilizando NodeJS
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+// Crear el servidor/aplicación de express
+
+const app = express();
+
+
+// GET
+
+app.get('/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'Todo salio bien',
+        uid: 69876
+    });
+
+});
+
+
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${ 4000 } `);
+});
+
+
